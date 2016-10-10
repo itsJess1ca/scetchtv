f60df83a6e80fff45ece7d9a0eb2c8dcd769a712
Jordan Ide <jorrdandied@hotmail.com>
2016-10-10 10:03:27 +0100

    actually fix spec tests!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('App: Web', () => {
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('IS THIS THING ON?!');
   }));
 
   it('should render title in a h1 tag', async(() => {
